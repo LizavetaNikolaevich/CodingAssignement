4735d4961bf5a3c4132e4c8ce8056e95bc45d8a9
Elizaveta Nikolaevich <elizaveta.nikolaevich@l568.local>
2020-10-17 21:59:00 +0300

    chrome config

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,14 +1,15 @@
 <?php
 
 use Behat\Behat\Context\Context;
-use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
+use Behat\Mink\Mink;
+use Behat\Mink\Session;
+use DMore\ChromeDriver\ChromeDriver;
 
-/**
- * Defines application features from the specific context.
- */
+
 class FeatureContext implements Context
 {
+    public $mink;
+    public $session;
     /**
      * Initializes context.
      *
@@ -18,5 +19,36 @@ class FeatureContext implements Context
      */
     public function __construct()
     {
+        $this->mink = new Mink(array(
+            'browser' => new Session(new ChromeDriver('http://localhost:9222', null, 'http://www.google.com'))
+        ));
+    
+        $this->mink->setDefaultSessionName('browser');
+
+        $this->session = $this->mink->getSession();
+        $this->session->start();
     }
+
+ /**
+     * @Given I am a Check24 user who hasn't logged in yet
+     */
+    public function iAmACheckUserWhoHasntLoggedInYet()
+    {
+        #throw new PendingException();
+    }
+
+    /**
+     * @When I go to :arg1
+     */
+    public function iGoTo($arg1)
+    {
+        $this->session->visit($arg1);
+        
+        $el1 = $this->session->getPage()->find('css', '.c24-cookie-consent-notice-buttons .c24-cookie-consent-button:last-child');
+        $el1->click();
+        $el2 = $this->session->getPage()->find('css', '[data-gtm-type="checkout"][data-gtm-value="buybox"]');
+        $el2->click();
+        $this->session->wait(5000);
+    }
+
 }
---
+++ features/checkout.feature
@@ -0,0 +1,18 @@
+Feature: Checkout process
+
+  Scenario: 
+    Given I am a Check24 user who hasn't logged in yet
+    When I go to "https://preisvergleich.check24.de/testprodukte-spedition/eine-tuete-luft.html"
+    # Then "Eine Tüte Luft" title is displayed
+     When I click "Eine Tüte Luft" button
+    # Then "Für die Bestellung benötigen wir Ihre E-Mail-Adresse" title is displayed
+     When I enter "lizaveta.nikolaevich@gmail.com" for the "Email" field
+     And I click "weiter" button
+    # Then "Willkommen" title is displayed
+    # And there is a "Password" field
+    # When I enter "qwerty123!" for the "Password" field
+    # And I click "anmelden" button
+    # Then "CHECK24 Punkte sammeln" title is displayed
+    # When I click "Nein, danke." radio-button
+    # And I click "weiter" button
+    # Then "https://shopping.check24.de/checkout/bestellung.html" page is opened
