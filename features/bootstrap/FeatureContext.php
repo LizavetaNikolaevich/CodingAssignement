46ad63cc084d49373e08e65f7ae52489b056a92e
Elizaveta Nikolaevich <elizaveta.nikolaevich@l568.local>
2020-10-18 16:05:27 +0300

    add testData and locators

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -5,22 +5,41 @@ use Behat\Mink\Mink;
 use Behat\Mink\Session;
 use DMore\ChromeDriver\ChromeDriver;
 
+require_once 'PHPUnit/Framework/Assert/Functions.php';
+
 
 class FeatureContext implements Context
 {
     public $mink;
     public $session;
     public static $arraySelectors = array(
+        "Accept" => '.c24-cookie-consent-notice-buttons.clearfix .c24-cookie-consent-button:last-child',
         "Eine Tüte Luft" => '[data-gtm-type="checkout"][data-gtm-value="buybox"]',
         "E-Mail-Adresse" => '#cl_login',
-        "weiter" => '#c24-uli-login-btn',
+        "fistWeiter" => '#c24-uli-login-btn',
         "Passwort" => '#cl_pw_login',
         "anmelden" => '#c24-uli-pw-btn',
+        "Nein, danke." => '',
+        "secondWeiter" => '#c24-uli-points-btn',
+        "vorname" => '#address-first-name-billing',
+        "Nachname" => '#address-last-name-billing',
+        "PLZ" => ' #address-po-number-billing',
+        "Ort" => '[for="address-city-billing"]',
+        "Straße" => '[for="address-street-billing"]',
+        "Nr" => '[for="address-house-number-billing"]',
+        "Telefonnummer" => '[for="address-phone-number-billing"]',
     );
 
     public static $testData = array(
         "Email" => 'lizaveta.nikolaevich@gmail.com',
         "password" => 'qwerty123!',
+        "First Name" => 'Liza',
+        "Surname" => 'Nikolaevich',
+        "Postcode" => '74232',
+        "place" => 'Minsk',
+        "road" => 'Krasnaya',
+        "No." => '11',
+        "Phone number" => '5402293',
     );
 
 
@@ -57,28 +76,34 @@ class FeatureContext implements Context
     public function iGoTo($arg1)
     {
         $this->session->visit($arg1);
-        $el1 = $this->session->getPage()->find('css', '.c24-cookie-consent-notice-buttons .c24-cookie-consent-button:last-child')->click();
     }
 
     /**
      * @When I click :arg1 button
      */
-    public function iClickButton($button)
+    public function iClickButton($locator)
     {
-        $el1 = $this->session->getPage()->find('css', self::$arraySelectors[$button]) ->click();
+        $el1 = $this->session->getPage()->find('css', self::$arraySelectors[$locator]) ->click();
     }
 
+        /**
+     * @Then :arg1 title is displayed
+     */
+    public function titleIsDisplayed($arg1)
+    {
+        \PHPUnit\Framework\Assert::assertEquals('heh2', 'heh');
+    }
+ 
     /**
      * @When I enter :arg1 to the :arg2 field
      */
-    public function iEnterToTheField($arg1, $arg2)
+    public function iEnterToTheField($data, $locator)
     {
-        $this->session->wait(2000);
-        $this->session->visit($this->session->getCurrentUrl());
-        $this->session->wait(5000);
-        $element = $this->session->getPage()->find('css', '#cl_login')->click();
-        $element->setValue('testtesttestest');
-        $this->session->wait(5000);
+        print($this->session->getCurrentUrl());
+        $this->getSession()->reload();
+
+        $element = $this->session->getPage()->find('css', self::$arraySelectors[$locator])->click();
+        $element->setValue(self::$testData[$data]);
     }
 }
 //$node_field = $page->findById('poney-button');
--- features/checkout.feature
+++ features/checkout.feature
@@ -3,16 +3,25 @@ Feature: Checkout process
   Scenario: 
     Given I am a Check24 user who hasn't logged in yet
     When I go to "https://preisvergleich.check24.de/testprodukte-spedition/eine-tuete-luft.html"
-    # Then "Eine Tüte Luft" title is displayed
-     When I click "Eine Tüte Luft" button
+    Then "Eine Tüte Luft" title is displayed
+    When I click "Accept" button
+    When I click "Eine Tüte Luft" button
     # Then "Für die Bestellung benötigen wir Ihre E-Mail-Adresse" title is displayed
      When I enter "Email" to the "E-Mail-Adresse" field
-     And I click "weiter" button
+     And I click "fistWeiter" button
     # Then "Willkommen" title is displayed
     # And there is a "Password" field
      When I enter "password" to the "Passwort" field
      And I click "anmelden" button
     # Then "CHECK24 Punkte sammeln" title is displayed
-    # When I click "Nein, danke." radio-button
-    # And I click "weiter" button
+    # When I click "Nein, danke." button
+     And I click "secondWeiter" button
     # Then "https://shopping.check24.de/checkout/bestellung.html" page is opened
+    When I enter "First Name" to the "vorname" field
+    And I click "Mr/Mrs" button
+    And I enter "Surname" to the "Nachname" field
+    And I enter "Postcode" to the "PLZ" field
+    And I enter "place" to the "Ort" field
+    And I enter "road" to the "Straße" field
+    And I enter "No." to the "Nr" field
+    And I enter "Phone number" to the "Telefonnummer" field
